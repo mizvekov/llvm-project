d22fe886a1ea2819b802f0bc25bbd483e6f057ad
Matheus Izvekov <mizvekov@gmail.com>
2022-09-17 18:07:28 +0200

    [clang] store sugared converted arguments on TemplateSpecializationType
    WIP - Not ready for review

--- clang/include/clang/AST/TypeLoc.h
+++ clang/include/clang/AST/TypeLoc.h
@@ -1713,7 +1713,7 @@ public:
   }
 
   unsigned getNumArgs() const {
-    return getTypePtr()->template_arguments().size();
+    return getTypePtr()->getSpecifiedArguments().size();
   }
 
   void setArgLocInfo(unsigned i, TemplateArgumentLocInfo AI) {
@@ -1725,7 +1725,7 @@ public:
   }
 
   TemplateArgumentLoc getArgLoc(unsigned i) const {
-    return TemplateArgumentLoc(getTypePtr()->template_arguments()[i],
+    return TemplateArgumentLoc(getTypePtr()->getSpecifiedArguments()[i],
                                getArgLocInfo(i));
   }
 
@@ -1761,7 +1761,7 @@ public:
     setTemplateNameLoc(Loc);
     setLAngleLoc(Loc);
     setRAngleLoc(Loc);
-    initializeArgLocs(Context, getTypePtr()->template_arguments(),
+    initializeArgLocs(Context, getTypePtr()->getSpecifiedArguments(),
                       getArgInfos(), Loc);
   }
 
