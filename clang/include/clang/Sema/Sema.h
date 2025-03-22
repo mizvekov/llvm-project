1a7cee2de26e57a64d6aa6587240dd96ab0d9ad6
Matheus Izvekov <mizvekov@gmail.com>
2025-03-22 17:39:37 -0300

    Revert "[Clang][Sema] Retain the expanding index for unevaluated type constraints (#109518)"
    This reverts commit 50e5411e4247421fd606f0a206682fcdf0303ae3.

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -11350,7 +11350,6 @@ public:
                             ConceptDecl *NamedConcept, NamedDecl *FoundDecl,
                             const TemplateArgumentListInfo *TemplateArgs,
                             TemplateTypeParmDecl *ConstrainedParameter,
-                            QualType ConstrainedType,
                             SourceLocation EllipsisLoc);
 
   bool AttachTypeConstraint(AutoTypeLoc TL,
