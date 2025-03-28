6e31920dbdb5a345208009a5cc808eb92fa7bc23
Matheus Izvekov <mizvekov@gmail.com>
2025-03-27 22:21:32 -0300

    [clang] improved preservation of template keyword
    This makes it so clang can better represent the abscence of
    the template keyword for a template which cannot be resolved
    due to a dependent prefix.
    
    The tracking of the template keyword is removed from the
    NestedNameSpecifier, and added to the last type node which had
    it missing, the DependentTemplateSpecializationType (DTST).
    
    The DTST and the DependentTemplateName are refactored to share
    most of their implementation.
    
    That refactoring along with the removal of the TypeSpecWithTemplate kind
    from the nested name specifiers amounts to a large amount of code
    removed, making this patch a small performance improvement overall.
    
    This will also enable future further simplifications.
    
    As a drive-by, this removes some special cases from the type printer,
    allowing template arguments within NestedNameSpecifiers to not have
    their nested names suppressed when printing.

--- clang/include/clang/AST/ASTContext.h
+++ clang/include/clang/AST/ASTContext.h
@@ -1835,15 +1835,14 @@ public:
                              TagDecl *OwnedTagDecl = nullptr) const;
   QualType getDependentNameType(ElaboratedTypeKeyword Keyword,
                                 NestedNameSpecifier *NNS,
-                                const IdentifierInfo *Name,
-                                QualType Canon = QualType()) const;
+                                const IdentifierInfo *Name) const;
 
   QualType getDependentTemplateSpecializationType(
-      ElaboratedTypeKeyword Keyword, NestedNameSpecifier *NNS,
-      const IdentifierInfo *Name, ArrayRef<TemplateArgumentLoc> Args) const;
+      ElaboratedTypeKeyword Keyword, const DependentTemplateStorage &Name,
+      ArrayRef<TemplateArgumentLoc> Args) const;
   QualType getDependentTemplateSpecializationType(
-      ElaboratedTypeKeyword Keyword, NestedNameSpecifier *NNS,
-      const IdentifierInfo *Name, ArrayRef<TemplateArgument> Args) const;
+      ElaboratedTypeKeyword Keyword, const DependentTemplateStorage &Name,
+      ArrayRef<TemplateArgument> Args, bool IsCanonical = false) const;
 
   TemplateArgument getInjectedTemplateArg(NamedDecl *ParamDecl) const;
 
@@ -2392,10 +2391,8 @@ public:
                                         bool TemplateKeyword,
                                         TemplateName Template) const;
 
-  TemplateName getDependentTemplateName(NestedNameSpecifier *NNS,
-                                        const IdentifierInfo *Name) const;
-  TemplateName getDependentTemplateName(NestedNameSpecifier *NNS,
-                                        OverloadedOperatorKind Operator) const;
+  TemplateName
+  getDependentTemplateName(const DependentTemplateStorage &Name) const;
   TemplateName getSubstTemplateTemplateParm(TemplateName replacement,
                                             Decl *AssociatedDecl,
                                             unsigned Index,
